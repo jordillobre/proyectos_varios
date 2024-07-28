bacd06ab7d23152126739b7427b33f8978b0f4bb
jordillobre <jordillobre@gmail.com>
2024-07-28 16:34:09 +0200

    retos programación

---
+++ first_week.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 4
+
+print("Operadores Aritméticos:")
+print("La suma de", num1, "+", num2, "=", num1+num2)
+print("La resta de", num1, "-", num2, "=", num1-num2)
+print("La división de", num1, "/", num2, "=", num1/num2)
+print("La división entera de", num1, "/", num2, "=", num1//num2)
+print("El modulo de", num1, "/", num2, "=", num1%num2)
+print("La multiplicación de", num1, "*", num2, "=", num1*num2)
+print("La potencia de", num1, "elevado a", num2, "=", num1**num2)

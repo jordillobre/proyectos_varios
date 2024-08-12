1914a823fe77d143727d79807b6e694e99173973
jordillobre <jordillobre@gmail.com>
2024-08-12 10:38:03 +0200

    Cambio de sistema de carpetas
    En vez de estar agrupados por tipo de lenguaje utilizado, ahora las carpetas están por lenguaje y en cada una están todos sus retos. Además en la versión C# se ha agregado un fichero para ejecutar el reto que se quiera visualizar.

---
+++ Retos programacion Mouredev/2023/versionC#/versionC#/retos2023.cs
@@ -0,0 +1,37 @@
+using fizzBuzz;
+using multiplicar;
+
+class Program{
+    static void Main(string[] args){
+
+        string[] listRetos ={"Reto 00: El famoso fizz buzz",
+                            "Reto 40: Tabla de multiplicar"};
+
+        for (int i=0; i< listRetos.Length; i++){
+            Console.WriteLine(listRetos[i]);
+        }
+
+
+        Console.WriteLine("¿Que reto quieres que se ejecute? ");
+        string input = Console.ReadLine();
+
+        try{
+            int opcionSel = int.Parse(input);
+
+            switch (opcionSel){
+                case 0:
+                    FizzBuzz.EjecutarFizzBuzz();
+                    break;
+                case 40:
+                    Multiplicar.EjecutarMultiplicar();
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Por favor, elige un número mostrado en la lista.");
+                    break;
+            }
+        }catch(FormatException){
+            Console.WriteLine("Error: Debes introducir un número entero válido.");
+        }
+   
+    }
+}
---
+++ Retos programacion Mouredev/2023/versionC#/versionC#/tablaMultiplicar.cs
@@ -0,0 +1,25 @@
+﻿//  Crea un programa que sea capaz de solicitarte un número y se encargue de imprimir su tabla de multiplicar entre el 1 y el 10.
+//  - Debe visualizarse qué operación se realiza y su resultado.
+//      Ej: 1 x 1 = 1
+//          1 x 2 = 2
+//          1 x 3 = 3
+//          ... 
+
+using System;
+
+namespace multiplicar;
+public class Multiplicar{
+    public static void EjecutarMultiplicar(){
+        Console.WriteLine("¿Qué tabla de multiplicar deseas conocer? ");
+        string valInput = Console.ReadLine();
+
+        try{
+            int num = int.Parse(valInput);
+            for(int val=1; val<=10; val++ ){
+                Console.WriteLine(num + " * " + val + " = " + (val * num));
+            }
+        }catch(FormatException){
+            Console.WriteLine("Error: Debes de introducir un número entero válido");
+        }
+    }
+}
